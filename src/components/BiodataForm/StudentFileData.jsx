61f634486a74db9f4d3aa55b7ef606b24adc7bef
Viery Nugroho <viery15102002@gmail.com>
2024-07-24 21:03:29 +0700

    init

--- src/components/BiodataForm/StudentFileData.jsx
+++ src/components/BiodataForm/StudentFileData.jsx
@@ -1,34 +1,40 @@
-import React, { useState } from "react";
-import { Input } from "../Ui/Form/Input";
-import { ErrMsg } from "../Error/ErrMsg";
+import React, { useState } from 'react';
+import { Input } from '../Ui/Form/Input';
+import { ErrMsg } from '../Error/ErrMsg';
 
-function StudentFileData(error) {
-  const [formData, setFormData] = useState();
+function StudentFileData({ error, setFormData, formData }) {
+	function handleFilePdf(e) {
+		// oper data ke form data yang ada pada Form.jsx
+		const { name, value, files } = e.target;
+		setFormData((prevFormData) => ({
+			...prevFormData,
+			[name]: value,
+			studentDocument: files ? files[0] : prevFormData.studentDocument, // Menambahkan file jika ada
+		}));
+	}
 
-  function handleFilePdf(e) {
-    setFormData(e.target.files[0]);
-    console.log(e.target.files);
-  }
-
-  return (
-    <div>
-      <div className="mb-3">
-        <label htmlFor="studentDocument" className="form-label">
-          Default file input example
-        </label>
-        <Input
-          type={"file"}
-          id="studentDocument"
-          name="studentDocument"
-          className={"form-control"}
-          onChange={handleFilePdf}
-          accept=".pdf"
-          multiple={false}
-        />
-        <ErrMsg msg={error.studentDocument} />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className='mb-3'>
+				<label
+					htmlFor='studentDocument'
+					className='form-label'
+				>
+					Default file input example
+				</label>
+				<Input
+					type={'file'}
+					id='studentDocument'
+					name='studentDocument'
+					className={'form-control'}
+					onChange={handleFilePdf}
+					accept='.pdf'
+					multiple={false}
+				/>
+				<ErrMsg msg={error.studentDocument} />
+			</div>
+		</div>
+	);
 }
 
 export default StudentFileData;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,163 +1,173 @@
-import { Input } from "../../components/Ui/Form/Input";
-import { useEffect, useState } from "react";
-import ImgGoogle from "../../../src/assets/images/img-icon-google.png";
-import { ErrMsg } from "../../components/Error/ErrMsg";
-import { loginData, validateLogin } from "../../Utils/validation";
-import { SignUp } from "./SignUp";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useNavigate } from "react-router";
-import axios from "axios";
-import Cookies from "js-cookie";
+import { Input } from '../../components/Ui/Form/Input';
+import { useEffect, useState } from 'react';
+import ImgGoogle from '../../../src/assets/images/img-icon-google.png';
+import { ErrMsg } from '../../components/Error/ErrMsg';
+import { loginData, validateLogin } from '../../Utils/validation';
+import { SignUp } from './SignUp';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export const Login = ({ title, props }) => {
-  const [modalRegister, setModalRegister] = useState(false);
-  const [error, setError] = useState({});
-  const [modalLoginOpen, setModalLoginOpen] = useState(true);
-  const navigate = useNavigate();
+	const [modalRegister, setModalRegister] = useState(false);
+	const [error, setError] = useState({});
+	const [modalLoginOpen, setModalLoginOpen] = useState(true);
+	const navigate = useNavigate();
 
-  const handleRegisterModal = () => {
-    setModalLoginOpen(false);
-    setModalRegister(true);
-  };
+	const handleRegisterModal = () => {
+		setModalLoginOpen(false);
+		setModalRegister(true);
+	};
 
-  const handleLoginModal = () => {
-    setModalLoginOpen(true);
-    setModalRegister(false);
-  };
+	const handleLoginModal = () => {
+		setModalRegister(false);
+		setModalLoginOpen(true);
+	};
 
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
+	const [form, setForm] = useState({
+		email: '',
+		password: '',
+	});
 
-  const handleInput = (e) => {
-    const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
+	const handleInput = (e) => {
+		const { name, value } = e.target;
+		setForm({
+			...form,
+			[name]: value,
+		});
+	};
 
-  const handlePostForm = async (data) => {
-    const formLogin = loginData(data);
+	const handlePostForm = async (data) => {
+		const formLogin = loginData(data);
 
-    if (validateLogin(form, setError)) {
-      try {
-        const res = await axios.post(
-          "http://localhost:3000/api/v1/auth/login",
-          formLogin
-        );
-        console.log("respone :", res);
-        if (res.status === 200) {
-            const {token} = res.data.results;
-            Cookies.set('token', token);
-            navigate("/formData"); //buat ke hal form
-          toast.success("Berhasil melakukan login", { delay: 800 });
-        } else {
-          toast.error("Terjadi kesalahan saat login", { delay: 800 });
-        }
-      } catch (error) {
-        toast.error("Gagal Login", { delay: 800 });
-      }
-    }
-  };
+		if (validateLogin(form, setError)) {
+			try {
+				const res = await axios.post('http://localhost:3000/api/v1/auth/login', formLogin);
+				console.log('respone :', res.status);
+				if (res.status === 200) {
+					const token = res.data._token;
+					Cookies.set('token', token);
+					navigate('/formData'); //buat ke hal form
+					toast.success('Berhasil melakukan login', { delay: 800 });
+				} else {
+					toast.error('Terjadi kesalahan saat login', { delay: 800 });
+				}
+			} catch (error) {
+				toast.error('Gagal Login', { delay: 800 });
+			}
+		}
+	};
 
-  useEffect(() => {
-    if (props && props.nextPage) {
-      const timerId = setTimeout(() => navigate(props.nextPage), 300);
-      return () => clearTimeout(timerId);
-    }
-  }, [navigate, props]);
+	useEffect(() => {
+		if (props && props.nextPage) {
+			const timerId = setTimeout(() => navigate(props.nextPage), 300);
+			return () => clearTimeout(timerId);
+		}
+	}, [navigate, props]);
 
-  return (
-    <>
-      {modalLoginOpen && (
-        <div
-          className="modal fade"
-          id="exampleModal"
-          tabIndex="-1"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h1
-                  className="modal-title fs-5"
-                  id="exampleModalLabel"
-                  style={{ color: "#816503" }}
-                >
-                  {title}
-                </h1>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                  onClick={() => setModalLoginOpen(false)}
-                ></button>
-              </div>
-              <div className="modal-body">
-                <div className="mb-3">
-                  <Input
-                    type={"email"}
-                    className={"form-control"}
-                    placeholder={"Masukkan email anda"}
-                    id="email"
-                    name="email"
-                    value={form.email}
-                    onChange={handleInput}
-                  />
-                  <ErrMsg msg={error.email} />
-                </div>
-                <div className="mb-3">
-                  <Input
-                    type="password"
-                    className={"form-control"}
-                    placeholder={"Masukkan password anda"}
-                    id="password"
-                    name="password"
-                    value={form.password}
-                    onChange={handleInput}
-                  />
-                </div>
-                <ErrMsg msg={error.password} />
-              </div>
-              <div className="modal-footer">
-                <div className="d-grid gap-2 col-6 mx-auto">
-                  <button
-                    className="btn btn-primary"
-                    type="submit"
-                    onClick={() => handlePostForm(form)}
-                  >
-                    Login
-                  </button>
-                  <button
-                    className="btn btn-primary"
-                    type="button"
-                    onClick={handleRegisterModal}
-                  >
-                    Register
-                  </button>
-                </div>
-              </div>
-              <p className="title-or text-center" style={{ color: "#000000" }}>
-                atau
-              </p>
-              <div className="d-flex justify-content-center mx-auto mb-3">
-                <button type="button" className="btn btn-outline-dark btn-sm ">
-                  <img className="img-google" src={ImgGoogle} alt="" /> &nbsp;
-                  Masuk dengan google
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-      {modalRegister && (
-        <SignUp title={"Silahkan Mendaftar !"} onClose={handleLoginModal} />
-      )}
-    </>
-  );
+	return (
+		<>
+			{modalLoginOpen && (
+				<div
+					className='modal fade'
+					id='exampleModal'
+					tabIndex='-1'
+					aria-labelledby='exampleModalLabel'
+					aria-hidden='true'
+				>
+					<div className='modal-dialog'>
+						<div className='modal-content'>
+							<div className='modal-header'>
+								<h1
+									className='modal-title fs-5'
+									id='exampleModalLabel'
+									style={{ color: '#816503' }}
+								>
+									{title}
+								</h1>
+								<button
+									type='button'
+									className='btn-close'
+									data-bs-dismiss='modal'
+									aria-label='Close'
+									onClick={() => setModalLoginOpen(false)}
+								></button>
+							</div>
+							<div className='modal-body'>
+								<div className='mb-3'>
+									<Input
+										type={'email'}
+										className={'form-control'}
+										placeholder={'Masukkan email anda'}
+										id='email'
+										name='email'
+										value={form.email}
+										onChange={handleInput}
+									/>
+									<ErrMsg msg={error.email} />
+								</div>
+								<div className='mb-3'>
+									<Input
+										type='password'
+										className={'form-control'}
+										placeholder={'Masukkan password anda'}
+										id='password'
+										name='password'
+										value={form.password}
+										onChange={handleInput}
+									/>
+								</div>
+								<ErrMsg msg={error.password} />
+							</div>
+							<div className='modal-footer'>
+								<div className='d-grid gap-2 col-6 mx-auto'>
+									<button
+										className='btn btn-primary'
+										type='submit'
+										onClick={() => handlePostForm(form)}
+									>
+										Login
+									</button>
+									<button
+										className='btn btn-primary'
+										type='button'
+										onClick={handleRegisterModal}
+									>
+										Register
+									</button>
+								</div>
+							</div>
+							<p
+								className='title-or text-center'
+								style={{ color: '#000000' }}
+							>
+								atau
+							</p>
+							<div className='d-flex justify-content-center mx-auto mb-3'>
+								<button
+									type='button'
+									className='btn btn-outline-dark btn-sm '
+								>
+									<img
+										className='img-google'
+										src={ImgGoogle}
+										alt=''
+									/>{' '}
+									&nbsp; Masuk dengan google
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+			{modalRegister && (
+				<SignUp
+					title={'Silahkan Mendaftar !'}
+					onClose={handleLoginModal}
+				/>
+			)}
+		</>
+	);
 };
